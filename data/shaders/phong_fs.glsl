6a4e9f0285a38f0f90daed4755fa351e4168927d
Tomikaza <tom.quareme@gmail.com>
2016-04-24 21:31:19 +0200

    MULTILAYERED TEXTURING WORKS!!!

--- data/shaders/phong_fs.glsl
+++ data/shaders/phong_fs.glsl
@@ -27,22 +27,37 @@ uniform DirectionalLight dirLight;
 
 void main()
 {
-    // TODO(Tom): Fix to no lighting on texture alpha of 0
-    vec4 textureDiffuseResult = mix(texture(textureDiffuse1, vertexUV), texture(textureDiffuse2, vertexUV), 0.3);
-    // Ambient
-    vec4 ambient = vec4(dirLight.ambient, 1.0) * textureDiffuseResult;
-  	
-    // Diffuse 
-    vec3 normal = normalize(vertexNormal);
-    vec3 dirLightDirection = normalize(-dirLight.direction);
-    float diffuseFactor = max(dot(normal, dirLightDirection), 0.0);
-    vec4 diffuse = vec4(dirLight.diffuse, 1.0) * diffuseFactor * textureDiffuseResult;
+    // Blending textures
+    vec4 diffuseColor2 = texture(textureDiffuse2, vertexUV);
+    vec4 textureDiffuseResult;
+    if (diffuseColor2.w > 0.0) {
+        textureDiffuseResult = mix(texture(textureDiffuse1, vertexUV), texture(textureDiffuse2, vertexUV), 1.0);
+    } else {
+        textureDiffuseResult = texture(textureDiffuse1, vertexUV);
+    }
     
-    // Specular
-    vec3 cameraDirection = normalize(cameraPosition - vertexWorldPosition);
-    vec3 reflectDirection = reflect(-dirLightDirection, normal);  
-    float specularFactor = pow(max(dot(cameraDirection, reflectDirection), 0.0), material.shine);
-    vec4 specular = vec4(dirLight.specular, 1.0) * (specularFactor * vec4(material.specular, 1.0));  
+    // No lighting on texture alpha of 0
+    if (textureDiffuseResult.w > 0.0)
+    {
+        // Ambient
+        vec4 ambient = vec4(dirLight.ambient, 1.0) * textureDiffuseResult;
+  	
+        // Diffuse 
+        vec3 normal = normalize(vertexNormal);
+        vec3 dirLightDirection = normalize(-dirLight.direction);
+        float diffuseFactor = max(dot(normal, dirLightDirection), 0.0);
+        vec4 diffuse = vec4(dirLight.diffuse, 1.0) * diffuseFactor * textureDiffuseResult;
+    
+        // Specular
+        vec3 cameraDirection = normalize(cameraPosition - vertexWorldPosition);
+        vec3 reflectDirection = reflect(-dirLightDirection, normal);  
+        float specularFactor = pow(max(dot(cameraDirection, reflectDirection), 0.0), material.shine);
+        vec4 specular = vec4(dirLight.specular, 1.0) * (specularFactor * vec4(material.specular, 1.0));  
         
-    fragColor =  ambient + diffuse + specular;
+        fragColor = ambient + diffuse + specular;
+    } 
+    else 
+    {
+        fragColor = vec4(0.0, 0.0, 0.0, 0.0);
+    }
 } 
